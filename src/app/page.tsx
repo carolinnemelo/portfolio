382d30b4f7105277fec498b434caa488d89d6d25
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2025-03-04 09:38:23 +0100

    Refactor homepage to use BuildingPage component for development status display

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,5 @@
+import { BuildingPage } from "@/features/home";
+
 export default function Home() {
-  return (
-    <main className="flex min-h-screen items-center justify-center bg-gray-900 text-white">
-      <div className="text-center">
-        <div className="mt-4 animate-fade-in animate-duration-2000">
-        </div>
-        <h1 className="text-3xl font-bold">
-        🚧 Site under development... 🚀
-        </h1>
-      </div>
-    </main>
-  );
+  return <BuildingPage />;
 }
---
+++ src/features/home/components/building-page.tsx
@@ -0,0 +1,13 @@
+export function BuildingPage () {
+   return (
+    <main className="flex min-h-screen items-center justify-center bg-gray-900 text-white">
+      <div className="text-center">
+        <div className="mt-4 animate-fade-in animate-duration-2000">
+        </div>
+        <h1 className="text-3xl font-bold">
+        🚧 Site under development... 🚀
+        </h1>
+      </div>
+    </main>
+  );
+}
