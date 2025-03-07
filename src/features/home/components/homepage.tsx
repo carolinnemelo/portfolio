6320c2e732e623b0be04b3b3173ddfa2f5fd9b1c
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2025-03-07 11:44:28 +0100

    Implement conditional rendering in Homepage component with buttons for feature flag

--- src/features/home/components/homepage.tsx
+++ src/features/home/components/homepage.tsx
@@ -1,5 +1,14 @@
+import { Button } from "@/components/ui/button";
 import { BuildingPage } from "./building-page";
+import { Main } from "./main";
 
 export function Homepage() {
-  return <BuildingPage />;
+  return process.env.FF_HOMEPAGE === "ON" ? (
+    <section className="flex flex-col items-center justify-center h-screen">
+    <Button variant="secondary">Minimalist</Button>
+    <Button>Dopamine</Button>
+    </section>
+  ) : (
+    <BuildingPage />
+  );
 }
