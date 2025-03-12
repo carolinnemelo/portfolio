ee4962c38abe0abf956007f1af8c3032f888fe13
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2025-03-12 14:24:22 +0100

    Add theme selection to Homepage component with state management and dynamic styling

--- src/features/home/components/homepage.tsx
+++ src/features/home/components/homepage.tsx
@@ -1,14 +1,21 @@
-import { Button } from "@/components/ui/button";
-import { BuildingPage } from "./building-page";
-import { Main } from "./main";
+  import { Button } from "@/components/ui/button";
+  import { BuildingPage } from "./building-page";
+  import { useState } from "react";
+  import { cva } from "class-variance-authority";
 
-export function Homepage() {
-  return process.env.FF_HOMEPAGE === "ON" ? (
-    <section className="flex flex-col items-center justify-center h-screen">
-    <Button variant="secondary">Minimalist</Button>
-    <Button>Dopamine</Button>
-    </section>
-  ) : (
-    <BuildingPage />
-  );
-}
+  export function Homepage() {
+    const [theme, setTheme] = useState("minimalist");
+    theme === "minimalist" ? 
+
+  const themeClasses = cva("",{variants: {theme:{ minimalist:"bg-red-600", dopamine:"bg-green-600"
+  }}})
+
+    return process.env.FF_HOMEPAGE === "ON" ? (
+      <section className={`${themeClasses}flex  flex-col items-center justify-center h-screen`}>
+      <Button onClick={()=>setTheme("minimalist")} variant="secondary">Minimalist</Button>
+      <Button onClick={()=>setTheme("dopamine")}>Dopamine</Button>
+      </section>
+    ) : (
+      <BuildingPage />
+    );
+  }
