9bb0327a79c1a42f7a752676a397662b8ec52f37
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2025-04-04 14:05:33 +0200

    Refactor theme handling in BuildingPage and ChooseTheme components; add marquee animation in globals.css

--- src/features/home/components/building-page.tsx
+++ src/features/home/components/building-page.tsx
@@ -1,16 +1,35 @@
-import { ChooseTheme } from "./choose-theme";
+// import { ChooseTheme } from "./choose-theme";
 
-export function BuildingPage () {
+export function BuildingPage() {
+  const text = " Under development •";
+
   return process.env.FF_NEW_BUILDING_PAGE === "ON" ? (
-    <main className="text-center">
-      <div >hello</div>
-      <ChooseTheme />
+    <main className="flex flex-col text-center h-screen justify-center bg-blue-400">
+      {/* <ChooseTheme /> */}
+      <div className="relative w-full overflow-hidden  bg-yellow-300 justify-self-center py-1 rotate-2">
+        <div className="flex overflow-hidden">
+          <div className="whitespace-nowrap animate-marquee">
+            {Array.from({ length: 10 }, (_, i) => (
+              <span key={i}>
+                {text}
+              </span>
+            ))}
+          </div>
+          <div className="whitespace-nowrap animate-marquee">
+            {Array.from({ length: 10 }, (_, i) => (
+              <span key={i}>
+                {text}
+              </span>
+            ))}
+          </div>
+        </div>
+      </div>
     </main>
   ) : (
     <main className="flex min-h-screen items-center justify-center bg-gray-900 text-white">
       <div className="text-center">
         <div className="mt-4 animate-fade-in animate-duration-2000"></div>
-        <h1 className="text-3xl font-bold">🚧 Site under development... 🚀</h1>
+        <h1 className="text-3xl font-bold">🚧 Under development... 🚀</h1>
       </div>
     </main>
   );
--- src/features/home/components/choose-theme.tsx
+++ src/features/home/components/choose-theme.tsx
@@ -3,8 +3,10 @@ import { Switch } from "@/components";
 import { useTheme } from "next-themes";
 
 export function ChooseTheme() {
-  const { setTheme, theme } = useTheme();
-
+  const { setTheme, theme} = useTheme();
+  if (!theme) {
+    return "light";
+  }
   return (
     <section>
       <Switch id="toggle-theme" checked={theme === "dark"} onCheckedChange={() => setTheme(theme === "light" ? "dark" : "light")} />
