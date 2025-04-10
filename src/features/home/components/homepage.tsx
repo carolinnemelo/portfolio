ed96d96a356a038c3d1a214c551fde2b14ff7d8f
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2025-04-10 10:27:59 +0200

    Fix Homepage component return statement to ensure proper rendering of Hero and SkillsIcons

--- src/features/home/components/homepage.tsx
+++ src/features/home/components/homepage.tsx
@@ -2,8 +2,10 @@ import { Hero } from "./hero";
 import { SkillsIcons } from "./skills-icons";
 
 export function Homepage() {
-  <main className="flex flex-col gap-6">
-    <Hero />
-    <SkillsIcons />
-  </main>;
+  return (
+    <main className="flex flex-col gap-6">
+      <Hero />
+      <SkillsIcons />
+    </main>
+  );
 }
