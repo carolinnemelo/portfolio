c6939c2641f3abfe51c0af28e34ddf59164d810f
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2025-04-10 10:22:13 +0200

    Update Homepage component to always render Hero and SkillsIcons; modify layout metadata for clarity and change secondary color in globals.css

--- src/features/home/components/homepage.tsx
+++ src/features/home/components/homepage.tsx
@@ -1,14 +1,9 @@
-import { BuildingPage } from "./building-page";
 import { Hero } from "./hero";
 import { SkillsIcons } from "./skills-icons";
 
 export function Homepage() {
-  return process.env.FF_HOMEPAGE === "ON" ? (
-    <main className="flex flex-col gap-6">
-      <Hero />
-      <SkillsIcons />
-    </main>
-  ) : (
-    <BuildingPage />
-  );
+  <main className="flex flex-col gap-6">
+    <Hero />
+    <SkillsIcons />
+  </main>;
 }
