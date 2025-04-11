63ac0aaa624499c34aee779127729a882c24d49e
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2025-04-11 22:19:16 +0200

    Add padding to main section in Homepage component

--- src/features/home/components/homepage.tsx
+++ src/features/home/components/homepage.tsx
@@ -1,11 +1,13 @@
+import { FoldableCard } from "./foldable-card";
 import { Hero } from "./hero";
 import { SkillsIcons } from "./skills-icons";
 
 export function Homepage() {
   return (
-    <main className="flex flex-col gap-6">
+    <main className="flex flex-col gap-6 py-32">
       <Hero />
       <SkillsIcons />
+      <FoldableCard />
     </main>
   );
 }
