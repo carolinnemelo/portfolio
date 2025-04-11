25eaff775d65ea74eb6bd2611ea27ffb5da67788
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2025-04-11 23:30:22 +0200

    Add alive.avif image to foldable card, rounded the edges and changed proportion

--- src/features/home/components/foldable-card.tsx
+++ src/features/home/components/foldable-card.tsx
@@ -10,7 +10,7 @@ import { useState } from "react";
 
 export function FoldableCard() {
   const [isFolded, setIsFolded] = useState(false);
-
+  const [isVisible, setIsVisible] = useState(false);
   const xDrag = useMotionValue(0);
   const xLeftSection = useTransform(xDrag, [0, 300], ["100%", "0%"]);
   const xRightSection = useTransform(xDrag, [0, 300], ["-100%", "0%"]);
@@ -25,8 +25,11 @@ export function FoldableCard() {
     }
   });
   return (
-    <section className="overflow-x-clip">
+    <section className="overflow-x-clip hidden md:block">
       <motion.div
+        onViewportEnter={() => {
+          setIsVisible(true);
+        }}
         animate={isFolded ? "open" : "folded"}
         variants={{
           open: { scale: 1 },
@@ -35,54 +38,61 @@ export function FoldableCard() {
         initial="folded"
         className="flex flex-col items-center"
       >
-        <motion.div
-          variants={{ open: { rotate: 0 }, hovering: { rotate: 0 } }}
-          whileHover="hovering"
-          initial={{ rotate: 3 }}
-          className="mx-auto grid aspect-video h-[80vh] p-8"
-        >
-          <div className="aspect-video h-full w-full grid grid-cols-3 [grid-area:1/1]">
+        {isVisible && (
+          <>
             <motion.div
-              style={{ x: xLeftSection, skewY: "-1deg" }}
-              className="map-image origin-bottom-right border-r border-zinc-500/20 shadow-2xl"
-            />
+              variants={{ open: { rotate: 0 }, hovering: { rotate: 0 } }}
+              whileHover="hovering"
+              initial={{ rotate: 3 }}
+              className="mx-auto grid aspect-video md:h-[80vh] p-8"
+            >
+              <div className="aspect-[7/5] h-full w-full grid grid-cols-3 [grid-area:1/1]">
+                <motion.div
+                  style={{ x: xLeftSection, skewY: "-1deg" }}
+                  className="map-image origin-bottom-right rounded-3xl border-r border-zinc-500/20 shadow-2xl"
+                />
+                <motion.div
+                  style={
+                    {
+                      scaleX: centerScale,
+                      "--brightness": centerBrightness,
+                      filter: "brightness(var(--brightness))",
+                    } as MotionStyle
+                  }
+                  className="map-image brightness-[--brightness] rounded-3xl"
+                />
+                <motion.div
+                  style={{ x: xRightSection, skewY: "1deg" }}
+                  className="map-image origin-bottom-left rounded-3xl border-l border-zinc-500/20 shadow-2xl"
+                />
+              </div>
+              <motion.div
+                drag="x"
+                className="[grid-area:1/1] relative z-10 cursor-grab active:cursor-grabbing"
+                _dragX={xDrag}
+                dragConstraints={{
+                  left: 0,
+                  right: 300,
+                }}
+                dragTransition={{
+                  modifyTarget: (target) => {
+                    return target > 150 ? 300 : 0;
+                  },
+                  timeConstant: 200,
+                }}
+              />
+            </motion.div>
             <motion.div
-              style={
-                {
-                  scaleX: centerScale,
-                  "--brightness": centerBrightness,
-                  filter: "brightness(var(--brightness))",
-                } as MotionStyle
-              }
-              className="map-image brightness-[--brightness]"
-            />
-            <motion.div
-              style={{ x: xRightSection, skewY: "1deg" }}
-              className="map-image origin-bottom-left border-l border-zinc-500/20 shadow-2xl"
-            />
-          </div>
-          <motion.div
-            drag="x"
-            className="[grid-area:1/1] relative z-10 cursor-grab active:cursor-grabbing"
-            _dragX={xDrag}
-            dragConstraints={{
-              left: 0,
-              right: 300,
-            }}
-            dragTransition={{
-              modifyTarget: (target) => {
-                return target > 150 ? 300 : 0;
-              },
-              timeConstant: 200,
-            }}
-          />
-        </motion.div>
-        <motion.div
-          variants={{
-            folded: { opacity: 0, scale: 0.9, y: 30 },
-            open: { opacity: 1, scale: 1, y: 0 },
-          }}
-        > CRAY CRAY</motion.div>
+              variants={{
+                folded: { opacity: 0, scale: 0.9, y: 30 },
+                open: { opacity: 1, scale: 1, y: 0 },
+              }}
+            >
+              {" "}
+              CRAY CRAY
+            </motion.div>
+          </>
+        )}
       </motion.div>
     </section>
   );
