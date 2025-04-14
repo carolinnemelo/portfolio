a23140e9f6f5ce56edc8cccfcb72c0d254693e28
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2025-04-14 15:03:51 +0200

    refactor: improve layout and spacing in Hero component

--- src/features/home/components/hero.tsx
+++ src/features/home/components/hero.tsx
@@ -1,9 +1,8 @@
-import { Button } from "@/components";
 import Image from "next/image";
 
 export function Hero() {
   return (
-    <section className="flex flex-col items-center justify-center gap-16 md:flex-row-reverse bg-secondary/50 py-16 md:py-32">
+    <section className="flex flex-col items-center justify-center gap-16 md:flex-row-reverse bg-secondary py-16 md:py-32 px-2 md:px-16">
       <Image
         src={"/carolinne.avif"}
         alt="Carolinne Melo"
@@ -13,13 +12,13 @@ export function Hero() {
         className="outline-2 outline-offset-16 rounded-full outline-background"
       />
 
-      <div className="flex flex-col justify-center px-4 pt-16 gap-6 w-[60%]">
-        <div className="space-y-2">
+      <div className="flex flex-col justify-center px-2 md:pt-16 gap-6 ">
+        <div className="space-y-4">
           <h1 className="text-4xl md:text-6xl font-bold font-accent ">
             {"Hi, I'm Carolinne"}
           </h1>
           <h2 className="font-[400] text-2xl">
-          I work with JavaScript, TypeScript, React, Next.js, Figma & more.
+              I work with JavaScript, TypeScript, React, Next.js, Figma & more.
           </h2>
         </div>
         <p className="text-xl text-neutral-800 font-[300]">
@@ -29,13 +28,13 @@ export function Hero() {
           <br />
           This site is my playground and my portfolio.
         </p>
-        <Button
+        {/* <Button
           size="lg"
           className="text-lg w-fit disabled:bg-muted disabled:text-muted-foreground"
           disabled
         >
           Explore My Work
-        </Button>
+        </Button> */}
       </div>
     </section>
   );
